69416f2413563689b059a15db0609066eded1d66
kevinmatoso001 <kevin.matoso@escola.pr.gov.br>
2025-11-17 10:33:56 -0300

    comit renovo

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -21,24 +21,48 @@ for (var i=0; i < pacientes.length; i++) {
     var altura = tdAltura.textContent;
     var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoEhValido = true;
-    var alturaEhValida = true;
-    //validar os dados de peso
-    if (peso <= 0 ||  peso >= 1000) {
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
+    
+    if (!pesoEhvalido) {
         console.log("Peso inválido");
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido";
     }
-    //validar a altura
-    if (altura <=0 || altura > 3.00){
+    
+    if (!alturaEhValida){
         console.log("Altura inválida");
         alturaEhValida = false;
         tdImc.textContent = "Altura inválida";
     }
-    //validar peso e altura
+    
     if (alturaEhValida && pesoEhValido) {
-        var imc = peso / (altura*altura);
-        tdImc.textContent = imc.toFixed(2);
+        var imc = calculaImc(peso,altura);
+        tdImc.textContent = imc;
     }
 
+}
+
+// Reescrever o mesmo código em tres funções independentes
+
+function calculaImc(peso, altura){
+    var imc = 0;
+    imc = peso/(altura * altura);
+    return imc.toFixed(2);
+}
+
+function validaPeso(peso){
+    if (peso <= 0 ||  peso >= 1000) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validaAltura(altura) {
+    if (altura <=0 || altura > 3.00){
+        return false;
+    } else {
+        return true;
+    }
 }
